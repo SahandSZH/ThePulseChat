6a79368dd33b6abc2a86c3f5074ca2af9ff91f81
Sahand Sadeghzadeh <153428343+SahandSZH@users.noreply.github.com>
2025-06-29 19:39:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import group
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="https://.*\.lovable\.app",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Create the tables if they don't exist already
 Base.metadata.create_all(bind=engine)
 
 app.include_router(group.router)
