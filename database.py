dd24c8c88b6ef980d8e4f95604eb45060c8e28d3
Sahand Sadeghzadeh <153428343+SahandSZH@users.noreply.github.com>
2025-06-29 03:53:48 -0400

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+load_dotenv("/etc/secrets/.env")
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
