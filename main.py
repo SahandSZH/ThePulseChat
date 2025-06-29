2551adfbf90ef16ad24de5ecc07c09f7e35c5587
Sahand Sadeghzadeh <153428343+SahandSZH@users.noreply.github.com>
2025-06-29 19:55:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from database import Base, engine
 from routers import group
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.lovable(project)?\.app|https://.*\.lovableproject\.com",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 Base.metadata.create_all(bind=engine)
 
